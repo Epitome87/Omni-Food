cf22919a487823fc0feeac1b3c4b33e9191485e6
Matthew McGrath <rubywep@hotmail.com>
2022-05-06 12:00:47 -0700

    Add transitions on elements scrolling into view
    Using IntersectionObserver in JavaScript to add fade-in animations to various elements. Will probably fine-tune these later, but for now it adds a sense of polish to the site.

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-// const navButton = document.querySelector('.button--mobile-nav');
+// Elements required for mobile navigation
 const navOpenButton = document.querySelector("[name='menu-outline']");
 const navCloseButton = document.querySelector("[name='close-outline']");
 const header = document.querySelector('.header');
 const navList = document.querySelector('.main-nav');
 
+// Elements required for Intersection Observer animations
+const revealElements = document.querySelectorAll('.reveal');
+
+// Functions required for mobile navigation
 function handleOpenNavigation(event) {
   header.classList.add('nav-open');
 }
@@ -16,6 +20,35 @@ function handleCloseNavigation(event) {
   }
 }
 
+// Creates IntersectionObserver and attaches it to appropriate elements
+function createObserver() {
+  let options = {
+    root: null,
+    rootMargin: '0px',
+    thresholds: '50px',
+  };
+
+  const observer = new IntersectionObserver(handleIntersect, options);
+
+  revealElements.forEach((revealElement) => observer.observe(revealElement));
+}
+
+let prevRatio = 0.0;
+
+function handleIntersect(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio <= 0) {
+      // Do this if we want the animation to occur EVERY time in view
+      // entry.target.classList.remove('active');
+    } else {
+      entry.target.classList.add('active');
+    }
+  });
+}
+
+// Event Listeners required for mobile navigation
 navOpenButton.addEventListener('click', handleOpenNavigation);
 navCloseButton.addEventListener('click', handleCloseNavigation);
 window.addEventListener('click', handleCloseNavigation);
+
+createObserver();
